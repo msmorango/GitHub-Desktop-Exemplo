bc3f63de772f7e0b08949cf21a230c5479efa5fe
Morango <math3usvalenca@gmail.com>
2025-12-01 14:01:35 -0300

    Revert "Hello World"
    This reverts commit a7bbf812b603155c48f0dba46e814b08e9c8a5be.

--- Github Desktop Example/src/github/desktop/example/GithubDesktopExample.java
+++ Github Desktop Example/src/github/desktop/example/GithubDesktopExample.java
@@ -1,10 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package github.desktop.example;
 
+/**
+ *
+ * @author AMD
+ */
 public class GithubDesktopExample {
 
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // TODO code application logic here
     }
     
 }
